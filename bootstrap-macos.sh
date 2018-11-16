5e83320025ecba4fd392eaf487962ca5686d77b5
Kevin Cooper <kevincooper@email.arizona.edu>
2018-11-16 16:39:04 -0700

    update bootstrap script

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # make directories
-if [[ ! -d '~/src' ]]; then
+if [[ ! -d ~/src ]]; then
   mkdir -p ~/src
 fi
 
+mkdir /tmp/bootstrap
+
 # gen ssh key
-if [[ ! -f '~/.ssh/id_rsa' ]]; then
+if [[ ! -f ~/.ssh/id_rsa ]]; then
   ssh-keygen -t rsa -b 4096
 fi
 
@@ -21,24 +23,24 @@ curl -L https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.g
 git config --global core.excludesfile ~/.gitignore
 
 # download iterm2
-curl -L https://iterm2.com/downloads/stable/latest > /tmp/iterm.zip
-unzip /tmp/iterm.zip -d /Applications
-
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -L https://iterm2.com/downloads/stable/latest > /tmp/bootstrap/iterm.zip
+unzip /tmp/bootstrap/iterm.zip -d /Applications
 
 # download iterm themes
 git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/src/iTerm2-Color-Schemes
 
+# download vscode
+curl -L https://update.code.visualstudio.com/latest/darwin/stable > /tmp/bootstrap/vscode.zip
+unzip /tmp/bootstrap/vscode.zip -d /Applications
+
+# download chrome
+curl -L https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg > /tmp/bootstrap/googlechrome.dmg
+hdiutil attach /tmp/bootstrap/googlechrome.dmg
+cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
+hdiutil detach `hdiutil info | grep "Google Chrome" | cut -f1 | grep -oe '.*disk[0-9]*'`
+
 # download powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-# download vscode
-curl -L https://update.code.visualstudio.com/latest/darwin/stable > /tmp/vscode.zip
-unzip /tmp/vscode.zip -d /Applications
-
-# download chrome
-curl -L https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg > /tmp/googlechrome.dmg
-hdiutil attach /tmp/googlechrome.dmg
-cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
-hdiutil detach `hdiutil info | grep "Google Chrome" | cut -f1 | grep -oe '.*disk[0-9]*'`
+# install oh-my-zsh (this needs to be last because it takes over the active shell)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
