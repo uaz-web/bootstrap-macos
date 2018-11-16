b042b74284cda0361418155905149251e471e4be
Kevin Cooper <kevincooper@email.arizona.edu>
2018-11-16 16:18:06 -0700

    updated script

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -1,46 +1,44 @@
 #!/bin/bash
 
 # make directories
-(
-  cd ~
-  mkdir src
-)
+if [[ ! -d '~/src' ]]; then
+  mkdir -p ~/src
+fi
 
 # gen ssh key
-ssh-keygen -t rsa -b 4096
+if [[ ! -f '~/.ssh/id_rsa' ]]; then
+  ssh-keygen -t rsa -b 4096
+fi
 
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # install brew packages
-brew install git
-brew install vim
-brew install node
+brew install -y git vim node
+
+# configure git
+curl -L https://raw.githubusercontent.com/github/gitignore/master/Global/macOS.gitignore > ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
 
 # download iterm2
 curl -L https://iterm2.com/downloads/stable/latest > /tmp/iterm.zip
-unzip /tmp/iterm.zip
-mv /tmp/iTerm.app /Applications/.
+unzip /tmp/iterm.zip -d /Applications
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # download iterm themes
-(
-  cd src
-  git clone https://github.com/mbadolato/iTerm2-Color-Schemes
-)
+git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/src/iTerm2-Color-Schemes
 
 # download powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-# download atom
-curl -L https://atom.io/download/mac > /tmp/atom.zip
-unzip /tmp/atom.zip
-mv /tmp/Atom.zip /Applications/.
+# download vscode
+curl -L https://update.code.visualstudio.com/latest/darwin/stable > /tmp/vscode.zip
+unzip /tmp/vscode.zip -d /Applications
 
 # download chrome
-(
-  cd Downloads
-  curl -LO https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
-)
+curl -L https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg > /tmp/googlechrome.dmg
+hdiutil attach /tmp/googlechrome.dmg
+cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
+hdiutil detach `hdiutil info | grep "Google Chrome" | cut -f1 | grep -oe '.*disk[0-9]*'`
